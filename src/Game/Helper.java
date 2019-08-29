5cd83f6564ddf794901469b2720efe4087ab54e5
JandeMul <jandemul2001@gmail.com>
2019-08-29 13:23:08 +0200

    add function controles

--- src/Game/Helper.java
+++ src/Game/Helper.java
@@ -12,6 +12,45 @@ public class Helper {
 //    {
 ////        return java.util.Random(from, to + 1);
 //    }
-}
 
+    while(true && areThereAnyDaysLeft())
+    {
+        String cmd = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+
+        switch (cmd)
+        {
+            // close command
+            case "close": case "quit": case "exit":
+            zoo.CloseApp();
+            break;
+
+            // feed penguin command
+            case "feed penguin":
+                zoo.FeedPenguin();
+                break;
+
+            // feed penguin command
+            case "feed polar bear":
+                zoo.FeedPolarBear();
+                break;
+
+            // help command
+            case "help": case "?":
+            zoo.ShowInstructions();
+            break;
+
+            // new day command
+            case "new day":
+                zoo.DayChange();
+                zoo.ShowStatus();
+                break;
+
+            // unknown command
+            default:
+                zoo.ShowWhatDoYouWant(cmd);
+                break;
+
+        }
+    }
+}
 
